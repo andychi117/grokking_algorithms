13b08df8e8973b8d72ef0579090a3e95732bd9c2
Andy Chi <andy.chi@canonical.com>
2023-12-25 22:54:42 +0800

    Initial commit

---
+++ ch1_binary_search/01_binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) //2
+        guess = list[mid]
+        
+        if item == guess:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+my_test_list = [1, 3, 5, 7, 9, 11, 13]
+
+print(binary_search(my_test_list, 3))
+
+print(binary_search(my_test_list, 1))
